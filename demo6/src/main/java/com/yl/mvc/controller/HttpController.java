0d058b95020fe2166ea398c213dd3abffbaec409
cui250384093 <cui250384093@163.com>
2021-11-24 16:20:51 +0800

    demo6

--- demo6/src/main/java/com/yl/mvc/controller/HttpController.java
+++ demo6/src/main/java/com/yl/mvc/controller/HttpController.java
@@ -1,6 +1,7 @@
 package com.yl.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HttpController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "/";
+    @RequestMapping("/testRequestBody")
+    public String testRequestBody(@RequestBody String requestBody) {
+        System.out.println(requestBody);
+        return "/success";
     }
 }
