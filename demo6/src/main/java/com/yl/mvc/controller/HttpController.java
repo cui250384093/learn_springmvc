534d5f7af68bbde1f8bf83d21dfb67d5497f5d19
cui250384093 <cui250384093@163.com>
2021-11-25 11:36:10 +0800

    demo6

--- demo6/src/main/java/com/yl/mvc/controller/HttpController.java
+++ demo6/src/main/java/com/yl/mvc/controller/HttpController.java
@@ -1,8 +1,15 @@
 package com.yl.mvc.controller;
 
+import com.yl.mvc.pojo.User;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.*;
 
 /**
  * @author candk
@@ -15,6 +22,55 @@ public class HttpController {
     @RequestMapping("/testRequestBody")
     public String testRequestBody(@RequestBody String requestBody) {
         System.out.println(requestBody);
-        return "/success";
+        return "success";
+    }
+
+    @RequestMapping("/testRequestEntity")
+    public String testRequestEntity(RequestEntity<String> requestEntity) {
+        System.out.println(requestEntity.getHeaders());
+        System.out.println(requestEntity.getBody());
+        return "success";
+    }
+
+    @RequestMapping("/testResponse")
+    public void testResponse(HttpServletResponse response) throws IOException {
+        response.getWriter().println("hello, response!");
+    }
+
+    @RequestMapping("/testResponseBody")
+    @ResponseBody
+    public String testResponseBody(){
+        return "success";
+    }
+
+    @RequestMapping("/testResponseUser")
+    @ResponseBody
+    public User testResponseUser(){
+        return new User(1001, "admin", "111", 23, "female");
+    }
+
+    @RequestMapping("/testDownload")
+    public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException {
+
+        ServletContext servletContext = session.getServletContext();
+        System.out.println(servletContext.getServletContextName());
+
+        String realPath = servletContext.getRealPath("/static/img/1.jpg");
+        System.out.println(realPath);
+
+        InputStream is = new FileInputStream(realPath);
+
+        byte[] bytes = new byte[is.available()];
+        is.read(bytes);
+
+        MultiValueMap<String, String> headers = new HttpHeaders();
+
+        headers.add("Content-Disposition", "attachment;filename=1.jpg");
+
+        HttpStatus statusCode = HttpStatus.OK;
+        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers, statusCode);
+
+        is.close();
+        return responseEntity;
     }
 }
---
+++ demo6/src/main/webapp/WEB-INF/templates/file.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: candk
+  Date: 11/25/21
+  Time: 9:45 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>File upload & download</title>
+</head>
+<body>
+    <h2>File</h2>
+    <a href="${pageContext.request.contextPath}/testUpload">testUpload</a><br/>
+    <a href="${pageContext.request.contextPath}/testDownload">testDownload</a><br/>
+</body>
+</html>
