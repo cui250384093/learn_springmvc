51e3db8089f7332db28088b125e073843553ece3
cui250384093 <cui250384093@163.com>
2021-11-25 16:32:17 +0800

    demo6

--- demo8/src/main/java/com/yl/mvc/interceptor/FirstInterceptor.java
+++ demo8/src/main/java/com/yl/mvc/interceptor/FirstInterceptor.java
@@ -30,6 +30,6 @@ public class FirstInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
-        System.out.println("afterApplication");
+        System.out.println("afterCompletion");
     }
 }
